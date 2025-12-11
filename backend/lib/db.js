d5eaebf416eab6102ed30776b0cf01cee838a80e
fari <fariyusuf368@gmail.com>
2025-12-12 01:57:56 +0700

    add timeout mongose

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			serverSelectionTimeoutMS: 5000, // default 1000 → naikkan
+			socketTimeoutMS: 45000, // untuk query lambat
+		});
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
