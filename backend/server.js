e963dee178053e029faabce25f11228c969dba7d
fari <fariyusuf368@gmail.com>
2025-12-12 01:03:12 +0700

    update axios

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 	console.log("Server is running on http://localhost:" + PORT);
 	connectDB();
 });
